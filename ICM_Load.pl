4b6bb903ca7b97c30242ef730791592964153085
Aingty Eung <aingtyeung@yahoo.com>
2018-02-14 15:50:36 -0800

    Bug Fixes

--- ICM_Load.pl
+++ ICM_Load.pl
@@ -4,9 +4,9 @@
 # Define I/O && initialize #
 $info = "\nICM_Load.pl opens and rename all files within a given folder into ICM. It will need the full directory path. \n\t(IE: ./ICM_Load.pl /home/server/ICM_FOLDER/ [new_name])";
 
-$input = "\nUsage\:  ICM_Load.pl [Your/.ob/Files/Folder] [new-name]\n (take away the [] when using this)
+$input = "\nUsage\:  ICM_Load.pl [Folder/Of/.ob/Files] [new-name]\n (take away the [] when using this)
 
-\t[Your/.ob/Files/Folder]\t\tPath to the .ob files\n\t[new-name]\t\tNew names due to ICM rename request
+\t[Folder/Of/.ob/Files]\t\tFolder to the .ob files\n\t[new-name]\t\tNew names due to ICM rename request
 ";
 # Set default values that can be overwritten #
 $directory = $ENV{'PWD'};
@@ -73,15 +73,7 @@ if($help==1)
 }
 
 # Validing user's directory #
-if(-e $directory)
-{
-  chomp $directory;
-  if((substr $directory, -1) ne "/")
-  {
-    $directory = $directory."/";
-  }
-}
-elsif (-e $ENV{'PWD'}.$directory)
+if (-e $ENV{'PWD'}.$directory)
 {
   $directory = $ENV{'PWD'}."/".$directory;
   chomp $directory;
